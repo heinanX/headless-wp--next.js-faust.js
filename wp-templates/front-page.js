c36bb310ed8fc59325d92a4704b39f25394d6e01
heinanX <larrii@live.se>
2023-11-21 11:35:44 +0100

    Co-authored-by: TOtterhall <TOtterhall@users.noreply.github.com>

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -1,31 +1,74 @@
 import { useQuery, gql } from '@apollo/client';
+import * as MENUS from '../constants/menus';
+import { BlogInfoFragment } from '../fragments/GeneralSettings';
+import {
+  Header,
+  Footer,
+  Main,
+  Container,
+  NavigationMenu,
+  Hero,
+  SEO,
+} from '../components';
 
 export default function Component() {
-  const { data } = useQuery(Component.query);
-  console.log(data);
+  const { data } = useQuery(Component.query, {
+    variables: Component.variables(),
+  });
   
+const { title: siteTitle, description: siteDescription } =
+    data?.generalSettings;
+  const primaryMenu = data?.headerMenuItems?.nodes ?? [];
+  const footerMenu = data?.footerMenuItems?.nodes ?? [];
 
   return (
     <>
-  
+  <SEO title={siteTitle} description={siteDescription} />
+      <Header
+        title={siteTitle}
+        description={siteDescription}
+        menuItems={primaryMenu}
+      />
+      <Main>
+        <Container>
+          <Hero title={'Front Page'} />
           <div className="text-center">
             <p>This page is utilizing the "front-page" WordPress template.</p>
             <code>this is b</code>
           </div>
-
+</Container>
+      </Main>
+      <Footer title={siteTitle} menuItems={footerMenu} />
     </>
   );
 }
 
 Component.query = gql`
-query pages {
-  pages {
+${BlogInfoFragment}
+  ${NavigationMenu.fragments.entry}
+  query GetPageData(
+    $headerLocation: MenuLocationEnum
+    $footerLocation: MenuLocationEnum
+  ) {
+    generalSettings {
+      ...BlogInfoFragment
+    }
+    headerMenuItems: menuItems(where: { location: $headerLocation }) {
     nodes {
-      editorBlocks {
-        name
-        renderedHtml
+      ...NavigationMenuItemFragment
       }
+    }
+    footerMenuItems: menuItems(where: { location: $footerLocation }) {
+      nodes {
+        ...NavigationMenuItemFragment
     }
   }
 }
 `;
+
+Component.variables = () => {
+  return {
+    headerLocation: MENUS.PRIMARY_LOCATION,
+    footerLocation: MENUS.FOOTER_LOCATION,
+  };
+};
