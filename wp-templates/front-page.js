7e1e8d23087f94f1bacb929eabcc391b53571d73
Sara Pellnor <114143422+SaraPellnor@users.noreply.github.com>
2023-11-24 09:54:03 +0100

    fixed frontpage with pageby

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -1,6 +1,6 @@
-import { useQuery, gql } from '@apollo/client';
-import * as MENUS from '../constants/menus';
-import { BlogInfoFragment } from '../fragments/GeneralSettings';
+import { useQuery, gql } from "@apollo/client";
+import * as MENUS from "../constants/menus";
+import { BlogInfoFragment } from "../fragments/GeneralSettings";
 import {
   Header,
   Footer,
@@ -9,42 +9,48 @@ import {
   NavigationMenu,
   Hero,
   SEO,
-} from '../components';
+  ContentWrapper,
+} from "../components";
 
+ 
 export default function Component() {
+
   const { data } = useQuery(Component.query, {
     variables: Component.variables(),
   });
-  
-const { title: siteTitle, description: siteDescription } =
+ 
+  const { title: siteTitle, description: siteDescription } =
     data?.generalSettings;
   const primaryMenu = data?.headerMenuItems?.nodes ?? [];
   const footerMenu = data?.footerMenuItems?.nodes ?? [];
-
+ 
+  const { title: pagetitle, content: content } = data?.pageBy;
+ 
   return (
     <>
-  <SEO title={siteTitle} description={siteDescription} />
+      <SEO title={siteTitle} description={siteDescription} />
       <Header
         title={siteTitle}
         description={siteDescription}
         menuItems={primaryMenu}
       />
+ 
       <Main>
-        <Container>
-          <Hero title={'Front Page'} />
-          <div className="text-center">
-            <p>This page is utilizing the "front-page" WordPress template.</p>
-            <code>this is b</code>
-          </div>
-</Container>
+        <Hero title={pagetitle} />
+        <>
+          {/* <EntryHeader title={pagetitle} /> */}
+          <Container>
+            <ContentWrapper content={content} />
+          </Container>
+        </>
       </Main>
       <Footer title={siteTitle} menuItems={footerMenu} />
     </>
   );
 }
-
+ 
 Component.query = gql`
-${BlogInfoFragment}
+  ${BlogInfoFragment}
   ${NavigationMenu.fragments.entry}
   query GetPageData(
     $headerLocation: MenuLocationEnum
@@ -54,21 +60,27 @@ ${BlogInfoFragment}
       ...BlogInfoFragment
     }
     headerMenuItems: menuItems(where: { location: $headerLocation }) {
-    nodes {
-      ...NavigationMenuItemFragment
+      nodes {
+        ...NavigationMenuItemFragment
       }
     }
     footerMenuItems: menuItems(where: { location: $footerLocation }) {
       nodes {
         ...NavigationMenuItemFragment
+      }
+    }
+    pageBy(uri: "/") {
+      id
+      title
+      content
     }
   }
-}
 `;
-
+ 
 Component.variables = () => {
   return {
     headerLocation: MENUS.PRIMARY_LOCATION,
     footerLocation: MENUS.FOOTER_LOCATION,
   };
 };
+ 
